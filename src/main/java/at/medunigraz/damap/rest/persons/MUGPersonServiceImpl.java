5061edd374febc2a70308b3edb7181f7bdd83b5a
David Eckhard <dabombeeer@gmail.com>
2024-07-22 16:55:44 +0200

    global: migrate to version 4.0.2

--- src/main/java/at/medunigraz/damap/rest/persons/MUGPersonServiceImpl.java
+++ src/main/java/at/medunigraz/damap/rest/persons/MUGPersonServiceImpl.java
@@ -1,51 +1,47 @@
 package at.medunigraz.damap.rest.persons;
 
-import java.util.List;
-import java.util.stream.Collectors;
-
-import javax.enterprise.context.ApplicationScoped;
-import javax.inject.Inject;
-import javax.ws.rs.core.MultivaluedMap;
-
-import org.eclipse.microprofile.rest.client.inject.RestClient;
-
-import at.ac.tuwien.damap.rest.base.ResultList;
-import at.ac.tuwien.damap.rest.base.Search;
-import at.ac.tuwien.damap.rest.dmp.domain.ContributorDO;
-import at.ac.tuwien.damap.rest.persons.PersonService;
 import at.medunigraz.api.rest.base.models.MUGSearchResult;
 import at.medunigraz.damap.rest.dmp.domain.MUGPerson;
 import at.medunigraz.damap.rest.dmp.mapper.MUGPersonDOMapper;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.core.MultivaluedMap;
+import java.util.List;
+import java.util.stream.Collectors;
 import lombok.extern.jbosslog.JBossLog;
+import org.damap.base.rest.base.ResultList;
+import org.damap.base.rest.base.Search;
+import org.damap.base.rest.dmp.domain.ContributorDO;
+import org.damap.base.rest.persons.PersonService;
+import org.eclipse.microprofile.rest.client.inject.RestClient;
 
 @JBossLog
 @ApplicationScoped
 public class MUGPersonServiceImpl implements PersonService {
 
-    @Inject
-    @RestClient
-    MUGPersonRestService restService;
+  @Inject @RestClient MUGPersonRestService restService;
 
-    @Override
-    public ContributorDO read(String id, MultivaluedMap<String, String> queryParams) {
-        MUGPerson contributor = restService.read(id, null);
+  @Override
+  public ContributorDO read(String id, MultivaluedMap<String, String> queryParams) {
+    MUGPerson contributor = restService.read(id, null);
 
-        return MUGPersonDOMapper.mapEntityToDO(contributor, new ContributorDO());
-    }
+    return MUGPersonDOMapper.mapEntityToDO(contributor, new ContributorDO());
+  }
 
-    @Override
-    public ResultList<ContributorDO> search(MultivaluedMap<String, String> queryParams) {
-        Search s = Search.fromMap(queryParams);
-        int limit = s.getPagination().getPerPage();
-        int offset = ((s.getPagination().getPage() < 1 ? 1 : s.getPagination().getPage()) - 1) * limit;
+  @Override
+  public ResultList<ContributorDO> search(MultivaluedMap<String, String> queryParams) {
+    Search s = Search.fromMap(queryParams);
+    int limit = s.getPagination().getPerPage();
+    int offset = ((s.getPagination().getPage() < 1 ? 1 : s.getPagination().getPage()) - 1) * limit;
 
-        MUGSearchResult<MUGPerson> people = restService.search(s.getQuery(), offset, limit, null);
+    MUGSearchResult<MUGPerson> people = restService.search(s.getQuery(), offset, limit, null);
 
-        List<ContributorDO> contributors = people.getResults().stream()
-                .map(c -> MUGPersonDOMapper.mapEntityToDO(c, new ContributorDO()))
-                .collect(Collectors.toList());
+    List<ContributorDO> contributors =
+        people.getResults().stream()
+            .map(c -> MUGPersonDOMapper.mapEntityToDO(c, new ContributorDO()))
+            .collect(Collectors.toList());
 
-        s.getPagination().setNumTotalItems(people.getCount());
-        return ResultList.fromItemsAndSearch(contributors, s);
-    }
+    s.getPagination().setNumTotalItems(people.getCount());
+    return ResultList.fromItemsAndSearch(contributors, s);
+  }
 }
--- src/test/java/at/medunigraz/damap/rest/PersonResourceTest.java
+++ src/test/java/at/medunigraz/damap/rest/PersonResourceTest.java
@@ -9,16 +9,6 @@ import static org.mockito.ArgumentMatchers.nullable;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
-import java.util.List;
-
-import org.eclipse.microprofile.rest.client.inject.RestClient;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
-
-import at.ac.tuwien.damap.rest.PersonResource;
-import at.ac.tuwien.damap.rest.persons.orcid.ORCIDPersonServiceImpl;
-import at.ac.tuwien.damap.security.SecurityService;
 import at.medunigraz.api.rest.base.models.MUGSearchResult;
 import at.medunigraz.damap.rest.dmp.domain.MUGPerson;
 import at.medunigraz.damap.rest.persons.MUGPersonRestService;
@@ -26,50 +16,56 @@ import io.quarkus.test.common.http.TestHTTPEndpoint;
 import io.quarkus.test.junit.QuarkusTest;
 import io.quarkus.test.junit.mockito.InjectMock;
 import io.quarkus.test.security.TestSecurity;
+import java.util.List;
+import org.damap.base.rest.PersonResource;
+import org.damap.base.rest.persons.orcid.ORCIDPersonServiceImpl;
+import org.damap.base.security.SecurityService;
+import org.eclipse.microprofile.rest.client.inject.RestClient;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
 @QuarkusTest
 @TestHTTPEndpoint(PersonResource.class)
 class PersonResourceTest {
 
-    @InjectMock
-    SecurityService securityService;
+  @InjectMock SecurityService securityService;
 
-    @InjectMock
-    @RestClient
-    MUGPersonRestService mockPersonRestService;
+  @InjectMock @RestClient MUGPersonRestService mockPersonRestService;
 
-    @InjectMock
-    ORCIDPersonServiceImpl orcidPersonServiceImpl;
+  @InjectMock ORCIDPersonServiceImpl orcidPersonServiceImpl;
 
-    @BeforeEach
-    public void setup() {
-        MUGPerson mockPerson = new MUGPerson();
-        mockPerson.setEmail("first.last@medunigraz.at");
-        mockPerson.setFirstName("first");
-        mockPerson.setLastName("last");
-        mockPerson.setId("medunigraz-first-last");
+  @BeforeEach
+  public void setup() {
+    MUGPerson mockPerson = new MUGPerson();
+    mockPerson.setEmail("first.last@medunigraz.at");
+    mockPerson.setFirstName("first");
+    mockPerson.setLastName("last");
+    mockPerson.setId("medunigraz-first-last");
 
-        MUGSearchResult<MUGPerson> mockSearchResult = new MUGSearchResult<>();
-        mockSearchResult.setCount(1);
-        mockSearchResult.setResults(List.of(mockPerson));
+    MUGSearchResult<MUGPerson> mockSearchResult = new MUGSearchResult<>();
+    mockSearchResult.setCount(1);
+    mockSearchResult.setResults(List.of(mockPerson));
 
-        Mockito.when(securityService.getUserId()).thenReturn("012345");
-        Mockito.when(securityService.getUserName()).thenReturn("testUser");
-        Mockito.when(mockPersonRestService.search(anyString(), anyInt(), anyInt(), nullable(List.class)))
-                .thenReturn(mockSearchResult);
-    }
+    Mockito.when(securityService.getUserId()).thenReturn("012345");
+    Mockito.when(securityService.getUserName()).thenReturn("testUser");
+    Mockito.when(
+            mockPersonRestService.search(anyString(), anyInt(), anyInt(), nullable(List.class)))
+        .thenReturn(mockSearchResult);
+  }
 
-    @Test
-    @TestSecurity(user = "userJwt", roles = "user")
-    void testSearchPeople() {
-        given()
-                .queryParams("searchService", "UNIVERSITY", "q", "")
-                .get("/")
-                .then()
-                .statusCode(200)
-                .body("items.size", not(0));
+  @Test
+  @TestSecurity(user = "userJwt", roles = "user")
+  void testSearchPeople() {
+    given()
+        .queryParams("searchService", "UNIVERSITY", "q", "")
+        .get("/")
+        .then()
+        .statusCode(200)
+        .body("items.size", not(0));
 
-        verify(mockPersonRestService, times(1)).search(anyString(), anyInt(), anyInt(), nullable(List.class));
-        verify(orcidPersonServiceImpl, times(0)).search(any());
-    }
+    verify(mockPersonRestService, times(1))
+        .search(anyString(), anyInt(), anyInt(), nullable(List.class));
+    verify(orcidPersonServiceImpl, times(0)).search(any());
+  }
 }
