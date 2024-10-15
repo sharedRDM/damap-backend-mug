c1fae41dc4860fc9470555425cc2e0b88942121c
Laura TH <lthaci@tugraz.at>
2024-10-15 14:06:24 +0200

    dependency: migrate to version v4.2.0

--- src/main/java/at/medunigraz/damap/rest/projects/MUGProjectRestService.java
+++ src/main/java/at/medunigraz/damap/rest/projects/MUGProjectRestService.java
@@ -1,23 +1,7 @@
 package at.medunigraz.damap.rest.projects;
 
-import at.medunigraz.api.rest.base.models.MUGSearchResult;
-import at.medunigraz.api.rest.base.services.MUGAPIServiceBase;
-import at.medunigraz.damap.rest.dmp.domain.MUGProject;
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.QueryParam;
-import java.time.Instant;
-import org.eclipse.microprofile.rest.client.annotation.ClientHeaderParam;
+import at.medunigraz.api.rest.base.services.MUGAPIProjectServiceBase;
 import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
 
 @RegisterRestClient(configKey = "rest.projects")
-public interface MUGProjectRestService extends MUGAPIServiceBase<MUGProject> {
-  @GET
-  @Path("")
-  @ClientHeaderParam(name = "accept", value = "application/json")
-  MUGSearchResult<MUGProject> search(
-      @QueryParam("title") String title,
-      @QueryParam("end_effective__gte") Instant endEffective,
-      @QueryParam("offset") int offset,
-      @QueryParam("limit") int limit);
-}
+public interface MUGProjectRestService extends MUGAPIProjectServiceBase {}
